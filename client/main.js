8c3fb4034065e640dd53b91838557d33a3275600
JP Lomas <jp@theqrl.org>
2020-11-29 23:21:50 +0000

    Tighter node connection logic & receipt check method scaffold

--- client/main.js
+++ client/main.js
@@ -1,25 +1,37 @@
-global.Buffer = global.Buffer || require("buffer").Buffer
-
 import './main.html'
+
+global.Buffer = global.Buffer || require('buffer').Buffer // eslint-disable-line
+
+let timerHandler = null
+
+const checkIfPaymentMade = () => {
+  Meteor.call('payment.received', document.getElementById('outputJson').textContent, (error, result) => {
+    console.log({ error, result })
+  })
+}
 
 Template.bodyContent.events({
   'click #submit-identity': () => {
     const id = document.getElementById('userId').value
     if (id.length > 0) {
-      Meteor.call('submitIdentity', id, function (error, result) {
-        console.log({error, result})
+      Meteor.call('submitIdentity', id, (error, result) => {
+        console.log({ error, result })
         if (!error) {
           document.getElementById('outputJson').textContent = result.hash
-          document.getElementById('modal').classList.add('is-active');
+          document.getElementById('modal').classList.add('is-active')
+          timerHandler = Meteor.setInterval(() => {
+            checkIfPaymentMade()
+          }, 6000)
         }
-      });
+      })
     }
-  }
+  },
 })
 
 Template.modal.events({
   'click .mc': () => {
     $('.modal').removeClass('is-active')
     $('html').removeClass('is-clipped')
-  }
+    Meteor.clearInterval(timerHandler)
+  },
 })
--- server/main.js
+++ server/main.js
@@ -1,90 +1,114 @@
 import { Meteor } from 'meteor/meteor'
 import { check } from 'meteor/check'
 import validate from '@theqrl/validate-qrl-address'
-import QrlNode from './qrlNode'
 import _ from 'underscore'
 import sha256 from 'sha256'
+import QrlNode from './qrlNode'
 
-const ip = 'testnet-1.automated.theqrl.org'
-const ip_mainnet = 'mainnet-1.automated.theqrl.org'
+const ip = 'testnet-2.automated.theqrl.org'
+// const ipMainnet = 'mainnet-1.automated.theqrl.org'
 const port = '19009'
 const testnet = new QrlNode(ip, port)
-const mainnet = new QrlNode(ip_mainnet, port)
+// const mainnet = new QrlNode(ipMainnet, port)
 const VENDOR_WALLET = 'Q01050038ca2264a06ee154a681201f99079bcd1ddf28355060b969daed25dd98efbf9ffd61b9b6'
 
-let vendorWalletTransactions = 0
+let doneStartup = false
+let vendorWalletTransactions = 0 // eslint-disable-line
 let vendorWalletIncomingTransactions = []
+
+// TODO
+//
+// * UI if network is down/not connected to QRL nodes
+//
+
+const getTx = (tx) => testnet.api('GetObject', { query: Buffer.from(tx, 'hex') }).then((res) => res)
+
+const howManyTx = (addr) => testnet
+  .api('GetOptimizedAddressState', { address: addr })
+  .then((res) => parseInt(res.state.transaction_hash_count, 10))
+
+const getAddrTx = (addr) => {
+  const apiAddr = Buffer.from(addr.substring(1), 'hex')
+  howManyTx(apiAddr).then((res) => {
+    console.log(`Vendor wallet has ${res} transactions`)
+    // check if this is greater than 'known' transactions
+    if (res > vendorWalletTransactions) {
+      // TO DO - logic if > 100
+      testnet
+        .api('GetMiniTransactionsByAddress', {
+          address: apiAddr,
+          item_per_page: 100,
+          page_number: 1,
+        })
+        .then((tx) => {
+          let incomingTx = 0
+          vendorWalletIncomingTransactions = []
+          _.each(tx.mini_transactions, (t) => {
+            if (t.out === false) {
+              incomingTx += 1
+              vendorWalletIncomingTransactions.push(t.transaction_hash)
+            }
+          })
+          console.log(`(of these Tx ${incomingTx} are incoming)`)
+          console.log('hashes:')
+          console.log(vendorWalletIncomingTransactions)
+          // let transfersIn = 0
+          _.each(vendorWalletIncomingTransactions, async (txD) => {
+            const txDetail = await getTx(txD)
+            if (txDetail.transaction.tx.transactionType === 'transfer') {
+              // transfersIn += 1
+              console.log(`Message: ${Buffer.from(txDetail.transaction.tx.transfer.message_data).toString()}`)
+            }
+          })
+        })
+    }
+  })
+}
 
 const checkConnectionStatus = () => {
   if (testnet.connection === true) {
     console.log('Testnet is connected OKAY')
+    if (!doneStartup) {
+      getAddrTx(VENDOR_WALLET)
+    }
   } else {
     console.log('ERROR: Testnet is not connected')
+    testnet.connect()
   }
-}
-
-const getTx = (tx) => {
-  return testnet.api('GetObject', { query: Buffer.from(tx, 'hex') }).then(res => {
-    return res
-  })
-}
-
-const getAddrTx = (addr) => {
-  const apiAddr = Buffer.from(addr.substring(1), 'hex')
-  testnet.api('GetOptimizedAddressState', { address: apiAddr }).then((res) => {
-    console.log('Vendor wallet has ' + res.state.transaction_hash_count + ' transactions')
-    // check if this is greater than 'known' transactions
-    if (res.state.transaction_hash_count > vendorWalletTransactions) {
-      // TO DO - logic if > 100
-      testnet.api('GetMiniTransactionsByAddress', {
-        address: apiAddr,
-        item_per_page: 100,
-        page_number: 1
-      }).then((tx) => {
-        let incomingTx = 0
-        vendorWalletIncomingTransactions = []
-        _.each(tx.mini_transactions, t => {
-          if (t.out === false) {
-            incomingTx += 1
-            vendorWalletIncomingTransactions.push(t.transaction_hash)
-          }
-        })
-        console.log('(of these Tx ' + incomingTx + ' are incoming)')
-        console.log('hashes:')
-        console.log(vendorWalletIncomingTransactions)
-        let transfersIn = 0
-        _.each(vendorWalletIncomingTransactions, async (tx) => {
-          const txDetail = await getTx(tx)
-          if (txDetail.transaction.tx.transactionType === 'transfer') {
-            transfersIn += 1
-            console.log('Message: ' + Buffer.from(txDetail.transaction.tx.transfer.message_data).toString('hex'))
-          }
-        })
-        // console.log('There were ' + transfersIn + ' transfers in')
-      })
-    }
-  })
 }
 
 Meteor.startup(() => {
   testnet.connect().then(() => {
     console.log('Connection attempt to Testnet')
-    console.log('Testnet connection status: ' + testnet.connection)
-    getAddrTx(VENDOR_WALLET)
-  });
-    Meteor.setInterval(() => {
+    console.log(`Testnet connection status: ${testnet.connection}`)
+    if (testnet.connection) {
+      doneStartup = true
+      getAddrTx(VENDOR_WALLET)
+    }
+  })
+  Meteor.setInterval(() => {
     checkConnectionStatus()
   }, 60000)
 
-    Meteor.methods({
-      submitIdentity(id) {
-        check(id, String)
-        if (validate.hexString(id).result) {
-          const sha256Addresses = sha256(id + VENDOR_WALLET)
-          return { message: 'GOOD ADDRESS', hash: sha256Addresses }
-        }
-        throw new Meteor.Error('Bad address')
+  Meteor.methods({
+    submitIdentity(id) {
+      check(id, String)
+      if (validate.hexString(id).result) {
+        const sha256Addresses = sha256(id + VENDOR_WALLET)
+        return { message: 'GOOD ADDRESS', hash: sha256Addresses }
       }
-    })
-
+      throw new Meteor.Error('Bad address')
+    },
+    'payment.received': function paymentReceived(id) {
+      check(id, String)
+      const apiAddr = Buffer.from(VENDOR_WALLET.substring(1), 'hex')
+      howManyTx(apiAddr).then((currTx) => {
+        console.log({ currTx })
+        if (currTx > vendorWalletTransactions) {
+          return 'maybe...'
+        }
+        return 'Not yet'
+      })
+    },
+  })
 })
